b1da5f5c275bffe7595728786bcbeb10fdcc3aa9
Shriya Deshmukh <shriya.deshmukh@seagate.com>
2022-05-05 06:48:48 -0600

    rgw_sal_motr: [CORTX-30178] Generate version-id based on timestamp
    Problem description:
    
    1. Object versions listed in random order in List Object Versions API
    2. is-latest flag is true for multiple versions if version-enabled bkt
       contains more than 30 versions of same object.
    3. is-latest flag is true for null and previous latest versioned obj if
       bkt versioning status changed from version enabled to suspended.
    
    Solution:
    
    1. To resolve 1st two issue generated timestamp based version-ids.
    2. To fix 3rd issue calling update_vesrion_entreis method in
       MotrAtomicWriter::complete fuc for both version suspended & enabled case.
    
    Signed-off-by: Shriya Deshmukh <shriya.deshmukh@seagate.com>

--- src/rgw/rgw_sal_motr.cc
+++ src/rgw/rgw_sal_motr.cc
@@ -18,6 +18,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <chrono>
 
 extern "C" {
 #pragma clang diagnostic push
@@ -65,6 +66,89 @@ static std::string motr_global_indices[] = {
 static unsigned roundup(unsigned x, unsigned by)
 {
   return ((x - 1) / by + 1) * by;
+}
+
+std::string base62_encode(uint64_t value, size_t pad)
+{
+  // Integer to Base62 encoding table. Characters are sorted in
+  // lexicographical order, which makes the encoded result
+  // also sortable in the same way as the integer source.
+  constexpr std::array<char, 62> base62_chars{
+      // 0-9
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+      // A-Z
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      // a-z
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+  std::string ret;
+
+  if (value == 0) {
+    ret = base62_chars[0];
+  }
+
+  while (value > 0) {
+    ret = base62_chars[value % base62_chars.size()] + ret;
+    value /= base62_chars.size();
+  }
+
+  if (ret.size() < pad) ret.insert(0, pad - ret.size(), base62_chars[0]);
+
+  return ret;
+}
+
+std::string base64_encode(unsigned char const* bytes_to_encode,
+                          unsigned in_len) {
+
+  const char base64_chars[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789.-";
+  std::string ret;
+  ret.reserve((in_len + 2) / 3 << 2);  // exact size
+
+  unsigned modulo_3 = 0;
+  int for_next = 0;  // -Wall
+
+  for (unsigned i = 0; i < in_len; ++i) {
+    const int cur_ch = *(bytes_to_encode + i);
+
+    switch (modulo_3) {
+      case 0:
+        ret += base64_chars[cur_ch >> 2 & 0x3F];
+        for_next = (cur_ch & 3) << 4;
+        break;
+      case 1:
+        assert(!(for_next & ~0x30));
+        ret += base64_chars[for_next | (cur_ch >> 4 & 0xF)];
+        for_next = (cur_ch & 0x0F) << 2;
+        break;
+      case 2:
+        assert(!(for_next & ~0x3C));
+        ret += base64_chars[for_next | (cur_ch >> 6 & 3)];
+        ret += base64_chars[cur_ch & 0x3F];
+        break;
+      default:
+        assert(0);
+    }
+    if (++modulo_3 > 2) {
+      modulo_3 = 0;
+    }
+  }
+  assert(modulo_3 < 3);
+
+  if (modulo_3) {
+    assert(!(for_next & ~0x3F));
+    ret += base64_chars[for_next];
+    ret += '=';
+
+    if (1 == modulo_3) {
+      ret += '=';
+    }
+  }
+  return ret;
 }
 
 void MotrMetaCache::invalid(const DoutPrefixProvider *dpp,
@@ -1378,9 +1462,31 @@ void MotrObject::gen_rand_obj_instance_name()
 {
   enum {OBJ_INSTANCE_LEN = 32};
   char buf[OBJ_INSTANCE_LEN + 1];
-
+  std::string version_id;
   gen_rand_alphanumeric_no_underscore(store->ctx(), buf, OBJ_INSTANCE_LEN);
-  key.set_instance(buf);
+  //TODO: Handle null version object case in PutObj operation.
+  // As the version ID timestamp is encoded in Base62, the maximum value
+  // for 8-characters is 62^8 - 1. This is the maximum time interval in ms.
+  constexpr uint64_t max_ts_count = 218340105584895;
+  constexpr size_t ts_len = 8, rand_id_len = 24;
+  using UnsignedMillis = std::chrono::duration<uint64_t, std::milli>;
+  const auto ms_since_epoch = std::chrono::time_point_cast<UnsignedMillis>(
+                              std::chrono::system_clock::now()).time_since_epoch().count();
+  uint64_t cur_time = max_ts_count - ms_since_epoch;
+  auto version_ts = base62_encode(cur_time, ts_len);
+  // encode random uuid string.
+  auto uuid_str = base64_encode(reinterpret_cast<const unsigned char*>(buf), rand_id_len);
+  // find and replace all "=" with "";
+  std::string search_str = "=", replace_str = "";
+  for (size_t pos = uuid_str.find(search_str, 0);
+    pos != std::string::npos && search_str.length();
+    pos = uuid_str.find(search_str, pos + replace_str.length()))
+      uuid_str.replace(pos, search_str.length(), replace_str);
+
+  version_id = version_ts + uuid_str;
+  if (version_id.length() > 31)
+    version_id = version_id.substr(0, 31);
+  key.set_instance(version_id);
 }
 
 int MotrObject::omap_get_vals(const DoutPrefixProvider *dpp, const std::string& marker, uint64_t count,
@@ -2568,6 +2674,7 @@ int MotrAtomicWriter::complete(size_t accounted_size, const std::string& etag,
   ent.meta.owner = owner.to_str();
   ent.meta.owner_display_name = obj.get_bucket()->get_owner()->get_display_name();
   RGWBucketInfo &info = obj.get_bucket()->get_info();
+  bool bkt_versioned =  obj.get_bucket()->get_info().versioned();
   if (info.versioning_enabled())
     ent.flags = rgw_bucket_dir_entry::FLAG_VER | rgw_bucket_dir_entry::FLAG_CURRENT;
   ldpp_dout(dpp, 20) <<__func__<< ": key=" << obj.get_key().to_str()
@@ -2591,7 +2698,7 @@ int MotrAtomicWriter::complete(size_t accounted_size, const std::string& etag,
   obj.meta.encode(bl);
   ldpp_dout(dpp, 20) <<__func__<< ": lid=0x" << std::hex << obj.meta.layout_id
                                                            << dendl;
-  if (info.versioning_enabled()) {
+  if (bkt_versioned) {
     // get the list of all versioned objects with the same key and
     // unset their FLAG_CURRENT later, if do_idx_op_by_name() is successful.
     // Note: without distributed lock on the index - it is possible that 2
